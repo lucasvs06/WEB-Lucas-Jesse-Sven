2610869c0b31f0759cb92260aec30bb86c9b1ba8
Jesboy3 <D304279@edu.curio.nl>
2024-11-18 10:02:11 +0100

    Homepage aangemaakt
    Zoals wireframe aangemaakt

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,3 +1,38 @@
 <x-base-layout>
-    <p>Hallo</p>
+    <main class="container mx-auto px-4 py-8">
+        <div class="grid grid-cols-3 gap-8">
+
+            <!-- Left Column -->
+            <div class="col-span-2 bg-white p-6">
+
+                <h2 class="text-2xl font-bold mb-2">Welkom</h2>
+                <p class="text-gray-700 pb-8">
+                    Deze website is gemaakt voor het zien van matches en het bieden op teams voor een leuke tijd.
+                </p>
+
+                <h3 class="font-bold text-lg ">Title 1</h3>
+                <p class="text-gray-700 pb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, beatae
+                    perspiciatis. Adipisci ea, odio iure ipsum harum, temporibus dolor aut nesciunt, numquam
+                    consequuntur cum enim deserunt repellendus impedit quaerat quod. Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Exercitationem reiciendis perspiciatis sunt illum. Sit reiciendis
+                    deleniti distinctio aliquam! In corrupti placeat illum odit amet, officia commodi asperiores facilis
+                    eaque est.
+                </p>
+
+                <h3 class="font-bold text-lg">Title 2</h3>
+                <p class="text-gray-700 pb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, beatae
+                    perspiciatis. Adipisci ea, odio iure ipsum harum, temporibus dolor aut nesciunt, numquam
+                    consequuntur cum enim deserunt repellendus impedit quaerat quod.
+                </p>
+
+
+            </div>
+
+            <!-- Right Column -->
+            <div class="bg-white border-solid border-2 border-black-700 rounded p-6">
+                <h2 class="text-lg font-bold mb-4">Leaderboard</h2>
+                <p class="text-gray-700">Who has the most coins!</p>
+            </div>
+        </div>
+    </main>
 </x-base-layout>
