3c6bf520125cc755840bd673c721460d397bc211
Jesboy3 <D304279@edu.curio.nl>
2024-12-02 08:55:19 +0100

    foutje

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('admin');
-            $table->unsignedInteger('team_id')->default(0);
+            $table->boolean('admin')->default(0);
+            $table->unsignedInteger('team_id');
             $table->rememberToken();
             $table->timestamps();
         });
