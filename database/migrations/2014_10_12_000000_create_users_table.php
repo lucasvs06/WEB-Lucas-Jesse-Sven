ec48aa9f881934920034e4684060dfa0cf3a53d1
Jesboy3 <D304279@edu.curio.nl>
2024-12-02 08:52:12 +0100

    team id migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('admin');
+            $table->unsignedInteger('team_id');
             $table->rememberToken();
             $table->timestamps();
         });
